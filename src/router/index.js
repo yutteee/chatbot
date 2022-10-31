3fd83b914fd25d75b3f8ce91350b6a8acbe9ec24
nakamura yusaku <nakamurayusaku@mba.local>
2022-10-31 17:57:49 +0900

    first setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '../components/views/LoginView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'loginView',
+    component: LoginView
   },
   {
     path: '/about',

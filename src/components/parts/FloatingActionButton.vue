31451a9a779bec903a334a86762328a575884e82
yutteee <yusaku_nakamura_aa@icloud.com>
2022-11-07 15:36:27 +0900

    complete button UI #4

--- src/components/parts/FloatingActionButton.vue
+++ src/components/parts/FloatingActionButton.vue
@@ -1,5 +1,7 @@
 <template>
-    <font-awesome-icon icon="fa-solid fa-message" />
+    <button class="float">
+        <font-awesome-icon icon="fa-regular fa-comment-dots" class="icon"/>
+    </button>
 </template>
 
 <script>
@@ -10,4 +12,22 @@ export default {
 </script>
 
 <style scoped>
+.float {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    background-color: #525F7F;
+    border: 0;
+    box-shadow: 4px 3px 2px rgb(149, 149, 149);
+    cursor: pointer;
+    position: fixed;
+    bottom: 64px;
+    right: 64px;
+}
+
+.icon {
+    width: 48px;
+    height: 48px;
+    color: #fff;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTwitter } from '@fortawesome/free-brands-svg-icons'
-import { faMessage } from '@fortawesome/free-solid-svg-icons'
+import { faCommentDots } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faTwitter, faMessage)
+library.add(faCommentDots)
 
 createApp(App).use(store).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')

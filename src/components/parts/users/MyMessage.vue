316f1260ee3ff4b886160d5667e1a80f7afefbe4
yutteee <yusaku_nakamura_aa@icloud.com>
2022-11-08 16:39:37 +0900

    myMessage & icon UI #6

--- src/components/parts/users/MyMessage.vue
+++ src/components/parts/users/MyMessage.vue
@@ -1,3 +1,32 @@
 <template>
-    <div>aa</div>
+    <div class="message">
+        How are you?
+        <img src="../../../assets/logo.png">
+    </div>
 </template>
+
+<style scoped>
+.message {
+    border: 1.5px solid #288BFF;
+    color: #288BFF;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    width: fit-content;
+    padding: 2px 5px;
+    box-shadow: 4px 2px 2px rgb(133, 133, 133);
+    position: relative;
+    margin-right: 40px;
+    margin-left: auto;
+    margin-top: 32px;
+}
+
+img {
+    border-radius: 50%;
+    width: 32px;
+    height: 32px;
+    position: absolute;
+    top: -20px;
+    right: -40px;
+}
+</style>

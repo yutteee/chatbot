e02f5d209b0506a9923d27bfc734edbaf52dc556
yutteee <yusaku_nakamura_aa@icloud.com>
2022-11-14 17:35:59 +0900

    completed tutrial #8

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,22 @@
 <div>
   <MyHeader></MyHeader>
   <form @submit.prevent="submitToken">
-  <input type="text" placeholder="Enter token" v-model="token" />
-  <button type="submit">Submit</button>
-</form>
+    <input type="text" placeholder="Enter token" v-model="token" />
+    <button type="submit">Submit</button>
+  </form>
+  <div class="box">
+  <div class="messages">
+    <div v-for="user in messages" :key="user.id">
+      {{user.name}}: {{user.message}}
+    </div>
+  </div>
+  <div class="messages"></div>
+  <form class="input-div" @submit.prevent="submitMessage">
+    <input type="text" placeholder="Type in text" 
+    v-model="inputMessageText" />
+    <button type="submit">Submit</button>
+  </form>
+</div>
   <router-view/>
 </div>
 </template>
@@ -13,6 +26,10 @@
 import MyHeader from './components/templates/MyHeader.vue'
 import SocketioService from './services/socketio.service.js';
 
+const SENDER = {
+    id: "123",
+    name: "John Doe",
+  };
 export default {
   components: {
     MyHeader
@@ -20,17 +37,56 @@ export default {
   data() {
     return {
       token: '',
+      inputMessageText: '',
+      messages: [],
     }
   },
   methods: {
-    submitToken : function () {
-      console.log(this.token);
-      // 本当はchatモーダル開いたときに接続したい
-      SocketioService.setupSocketConnection(this.token);
-    }
-  },
+      submitToken() {
+        console.log(this.token);
+        SocketioService.setupSocketConnection(this.token);
+        SocketioService.subscribeToMessages((err, data) => {
+          console.log(data);
+          this.messages.push(data);
+        });
+      },
+      submitMessage() {
+        const CHAT_ROOM = "myRandomChatRoomId";
+        const message = this.inputMessageText;
+        SocketioService.sendMessage({ message, roomName: CHAT_ROOM }, (cb) => {
+          // callback is acknowledgement from server
+          console.log(cb);
+          this.messages.push({
+            message,
+            ...SENDER,
+          });
+          // clear the input after the message is sent
+          this.inputMessageText = "";
+        });
+      },
+    },
   beforeUnmount() {
     SocketioService.disconnect();
   },
 }
 </script>
+
+<style scoped>
+.box {
+  width: fit-content;
+  height: 400px;
+  border: solid 1px #000;
+  display: flex;
+  flex-direction: column;
+  margin-top: 1rem;
+}
+
+.messages {
+  flex-grow: 1;
+}
+
+.input-div {
+  display: flex;
+  width: 100%;
+}
+</style>
--- src/services/socketio.service.js
+++ src/services/socketio.service.js
@@ -1,28 +1,39 @@
 import { io } from 'socket.io-client';
 
 class SocketioService {
-    socket;
-    constructor() {}
+  socket;
+  constructor() {}
 
-    setupSocketConnection(token) {
-        this.socket = io(process.env.VUE_APP_SOCKET_ENDPOINT);
-        this.socket.emit('my message', 'Hello there from Vue.');
-        this.socket.on('my broadcast', (data) => {
-            console.log(data);
-        });
-        this.socket = io(process.env.VUE_APP_SOCKET_ENDPOINT, {
-            auth: {
-                token,
-            }
-        });
-        console.log(`Connecting socket...`);
-    }
+  setupSocketConnection(token) {
+    this.socket = io(process.env.VUE_APP_SOCKET_ENDPOINT, {
+      auth: {
+        token,
+      },
+    });
+    console.log(`Connecting socket...`);
+    
+    this.socket.on('my broadcast', (data) => {
+      console.log(data);
+    });
+  }
 
-    disconnect() {
-        if (this.socket) {
-            this.socket.disconnect();
-        }
+  subscribeToMessages(cb) {
+    if (!this.socket) return(true);
+    this.socket.on('message', msg => {
+      console.log('Room event received!');
+      return cb(null, msg);
+    });
+  }
+  
+  sendMessage({message, roomName}, cb) {
+    if (this.socket) this.socket.emit('message', { message, roomName }, cb);
+  }
+  
+  disconnect() {
+    if(this.socket) {
+      this.socket.disconnect();
     }
+  }
 }
 
 export default new SocketioService();

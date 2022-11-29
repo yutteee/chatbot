21a557673ea10a759bbee315dd9fb95cbee56a71
yutteee <yusaku_nakamura_aa@icloud.com>
2022-11-29 15:13:08 +0900

    creacre ImagePreview Component #7

---
+++ src/components/parts/users/ImagePreview.vue
@@ -0,0 +1,22 @@
+<template>
+    <img v-if="previewUrl !== null" v-bind:src="previewUrl" class="preview-img">
+    <img v-else src="../../../assets/logo.png" class="preview-img">
+    <button @click="$emit('deletePreview')">a</button>
+</template>
+
+<script>
+export default {
+    emits:['deletePreview'],
+    props: {
+        previewUrl: String,
+    }
+}
+
+</script>
+
+<style scoped>
+.preview-img {
+    max-width: 100px;
+    max-height: 60px;
+}
+</style>

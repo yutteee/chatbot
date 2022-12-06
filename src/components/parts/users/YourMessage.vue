b5e2eea21f5f030d36ff1e09b1d80eb8349c9f7d
yutteee <yusaku_nakamura_aa@icloud.com>
2022-12-06 17:43:01 +0900

    Adjust css for display of files, etc #7

--- src/components/parts/users/YourMessage.vue
+++ src/components/parts/users/YourMessage.vue
@@ -8,25 +8,26 @@
             />
             <div 
                 v-else-if="determineFileType(fileType, 'text/')"
-                class="text"
                 @click="openFile(fileURL)"
-            >text file
+                class="file-text"
+            ><font-awesome-icon icon="fa-regular fa-file-lines" class="icon"/>
+            plain Text
             </div>
             <div 
                 v-else-if="determineFileType(fileType, 'application/pdf')"
-                class="text"
                 @click="openFile(fileURL)"
-            >PDF file
+                class="file-text"
+            ><font-awesome-icon icon="fa-regular fa-file-pdf" class="icon"/>
+            PDF file
             </div>
             <div 
                 v-else
-                class="text"
                 @click="openFile(fileURL)"
             >other
             </div>
         </div>
         <div class="text" v-else>{{message}}</div>
-        <img src="../../../assets/logo.png">
+        <img class="profile" src="../../../assets/logo.png">
     </div>
 </template>
 
@@ -53,6 +54,7 @@ export default {
 .message {
     position: relative;
 }
+
 .text {
     background-color: #F5F5F5;
     color: #636363;
@@ -66,20 +68,39 @@ export default {
     word-wrap: break-word;
 }
 
-.file {
+.sendedImg {
+    max-width: 280px;
+    box-shadow: 4px 2px 2px rgb(133, 133, 133);
+    margin:32px 40px 5px auto;
+    display: block;
+}
+
+.icon {
+    color: #525F7F;
+    width: 26px;
+    height: 26px;
+    padding: 5px;
+}
+
+.file-text {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
     background-color: #F5F5F5;
     color: #636363;
     border-top-right-radius: 20px;
     border-bottom-left-radius: 20px;
     border-bottom-right-radius: 20px;
-    width: fit-content;
-    max-width: 280px;
     padding: 2px 10px;
-    margin:32px auto 5px 40px;
-    word-wrap: break-word;
 }
 
-img {
+.file {
+    width: fit-content;
+    max-width: 280px;
+    margin:32px auto 5px 40px;
+}
+
+.profile {
     border-radius: 50%;
     width: 32px;
     height: 32px;

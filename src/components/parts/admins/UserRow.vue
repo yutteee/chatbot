ac5be127e6e1fe66a13b88f3e85e7c844ddf39d1
yutteee <yusaku_nakamura_aa@icloud.com>
2022-12-16 14:50:41 +0900

    Made it possible to pass data back and forth #35

--- src/components/parts/admins/UserRow.vue
+++ src/components/parts/admins/UserRow.vue
@@ -1,6 +1,16 @@
 <template>
-    <div>aaa</div>
+    <div>
+        {{userName}}
+    </div>
 </template>
 
 <script>
+export default {
+    props: {
+        userId: Number,
+        userName: String,
+        userImg: String,
+        userBirthDay: Date
+    }
+}
 </script>
--- src/components/templates/UsersList.vue
+++ src/components/templates/UsersList.vue
@@ -1,5 +1,12 @@
 <template>
-    <UserRow></UserRow>
+    <UserRow 
+        v-for="user in users" 
+        :key="user.id"
+        :userId="user.id"
+        :userName="user.name"
+        :userImg="user.img"
+        :userBirthDay="user.birthday"
+    ></UserRow>
 </template>
 
 <script>
@@ -8,6 +15,24 @@ import UserRow from '../parts/admins/UserRow.vue'
 export default {
     components :{
         UserRow
+    },
+    data() {
+        return {
+            users: [
+                {
+                    id: 1,
+                    name: "yusaku",
+                    image: "logo.png",
+                    birthday: Date(2002, 6, 12)
+                },
+                {
+                    id: 2,
+                    name: "nakamura",
+                    image: "logo.png",
+                    birthday: Date(2000, 6, 2)
+                },
+            ]
+        }
     }
 }
 

1515b11b77191348e20b42694a2d600255c206cc
yutteee <yusaku_nakamura_aa@icloud.com>
2022-12-16 16:34:11 +0900

    Search for the user on the user details page #35

--- src/components/views/AdminUserDetail.vue
+++ src/components/views/AdminUserDetail.vue
@@ -1,7 +1,34 @@
 <template>
-<div>{{this.$route.params.id}}</div>
+<div>{{user.name}}</div>
 </template>
 
 <script>
-
+export default {
+    data() {
+        return {
+            users: [
+                {
+                    id: 1,
+                    name: "yusaku",
+                    image: "logo",
+                    birthday: "2002/07/12"
+                },
+                {
+                    id: 2,
+                    name: "nakamura",
+                    image: "logo",
+                    birthday: "2000/02/01"
+                },
+            ],
+            user: {}
+        }
+    },
+    created () {
+        const strId = this.$route.params.id;
+        const intId = Number(strId);
+        const searchedUser = this.users.find(({ id }) => id === intId);
+        console.dir(searchedUser)
+        this.user = searchedUser;
+    }
+}
 </script>
